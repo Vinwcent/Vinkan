13d9418698b3eee4e89154520f58560364d7901d
Vincent <91033856+Vinwcent@users.noreply.github.com>
2026-04-11 13:43:25 +0200

    fix: add stdexcept

--- src/vinkan/utils/memory_types.hpp
+++ src/vinkan/utils/memory_types.hpp
@@ -1,10 +1,13 @@
 #ifndef VINKAN_MEMORY_TYPES_HPP
 #define VINKAN_MEMORY_TYPES_HPP
 
-inline uint32_t getMemoryTypeIndex(
-    uint32_t typeFilter,
-    VkPhysicalDeviceMemoryProperties deviceMemoryProperties,
-    VkMemoryPropertyFlags bufferMemoryPropFlags) {
+#include <stdexcept>
+#include <vulkan/vulkan_core.h>
+
+inline uint32_t
+getMemoryTypeIndex(uint32_t typeFilter,
+                   VkPhysicalDeviceMemoryProperties deviceMemoryProperties,
+                   VkMemoryPropertyFlags bufferMemoryPropFlags) {
   for (uint32_t i = 0; i < deviceMemoryProperties.memoryTypeCount; i++) {
     if ((typeFilter & (1 << i)) &&
         (deviceMemoryProperties.memoryTypes[i].propertyFlags &
